ad30c4b5dc8c3c866226fc500114af155dbc3953
apase95 <noobyhandsome@gmail.com>
2026-03-10 20:54:59 +0700

    ADD: New Feature Sent Email BullMQ

--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -1,13 +1,30 @@
+import { InjectQueue } from "@nestjs/bullmq";
 import { Injectable, Logger } from "@nestjs/common";
+import { Queue } from "bullmq";
 
 
 @Injectable()
 export class MailService {
     private readonly logger = new Logger(MailService.name);
 
+    constructor(
+        @InjectQueue("mail-queue") private readonly mailQueue: Queue,
+    ){}
+
     async sendEmail(to: string, subject: string, html: string) {
-        this.logger.log(`[MOCK EMAIL] To: ${to} | Subject: ${subject}`);
-        this.logger.log(`[CONTENT]: ${html}`);
+        const job = await this.mailQueue.add(
+            "send-email-job",
+            { to, subject, html },
+            {
+                attempts: 3,
+                backoff: {
+                    type: "exponential",
+                    delay: 2000,
+                },
+                removeOnComplete: true,
+            },
+        );
+        this.logger.log(`[JOB ADDED] Job ID: ${job.id}`);
         return true;
     }
 }
